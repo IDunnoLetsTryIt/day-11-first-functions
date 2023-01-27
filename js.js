4f60fa230868ba69d7d11a68cbbf11c4ab540492
Klaus <kriegle@seznam.cz>
2023-01-27 06:31:22 +0100

    first function practice

---
+++ js.js
@@ -0,0 +1,7 @@
+function set_headline(){
+    document.querySelector("#headline").innerHTML="Javascript Functions";
+   
+}
+function oneMore(id,text){
+document.getElementById(id).innerHTML=text;
+}   
